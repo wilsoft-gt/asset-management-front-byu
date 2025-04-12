2bc98d24a570da0218207620d321ec61aebf7f07
wilsoft-gt <wilson.romero1992@gmail.com>
2025-04-11 23:52:28 -0600

    Fix bad redirect from asset create to asset list on cancel creation

--- src/views/AssetCreate.jsx
+++ src/views/AssetCreate.jsx
@@ -55,7 +55,7 @@ export const AssetCreate = () => {
       <Separator />
       <div className="flex gap-4">
         <button form="createForm" className="mb-4 min-w-30 bg-blue-500 pt-1 pb-1 rounded-sm text-white hover:shadow-md hover:bg-neutral-600 hover:text-neutral-100 cursor-pointer flex justify-center items-center gap-2"><IconPlus size={18} /> Create</button>
-        <NavLink to="/assets" end className="mb-4 min-w-30 bg-red-500 pt-1 pb-1 rounded-sm text-white hover:shadow-md hover:bg-neutral-600 hover:text-neutral-100 cursor-pointer flex justify-center items-center gap-2" onClick={() => navigator("/users")}><IconX size={18} /> Cancel</NavLink>
+        <NavLink to="/assets" end className="mb-4 min-w-30 bg-red-500 pt-1 pb-1 rounded-sm text-white hover:shadow-md hover:bg-neutral-600 hover:text-neutral-100 cursor-pointer flex justify-center items-center gap-2"><IconX size={18} /> Cancel</NavLink>
       </div>
     </section>
   )
